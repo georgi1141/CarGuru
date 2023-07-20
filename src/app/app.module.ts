3ccb1c0439b596da058c4483dbad49a91b2c56ad
georgi1141 <g.zh.yanev@gmail.com>
2023-07-20 13:26:42 +0200

    Connecting firebase to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { CoreModule } from './core/core.module';
+import {AngularFireModule} from '@angular/fire/compat';
+import { environment } from 'src/environments/environment.development';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    CoreModule
+    CoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyC7qavyNCHSpOi3SABjzq2ckDwX9nh9sos',
+    authDomain: 'carguru-ec389.firebaseapp.com',
+    databaseURL:
+      'https://carguru-ec389-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'carguru-ec389',
+    storageBucket: 'carguru-ec389.appspot.com',
+    messagingSenderId: '371037706548',
+    appId: '1:371037706548:web:3fb982420cce6599aced61',
+    measurementId: 'G-2GEFX3JV2G',
+  },
+};
