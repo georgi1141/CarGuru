643719b0b7a2981f3da6eb98dfd22e0d11a671ab
georgi1141 <g.zh.yanev@gmail.com>
2023-07-20 14:46:05 +0200

    add components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { FormsModule } from '@angular/forms';
 import { CoreModule } from './core/core.module';
 import {AngularFireModule} from '@angular/fire/compat';
 import { environment } from 'src/environments/environment.development';
+import { HomeComponent } from './home/home.component';
+import { UserRoutingModule } from './user/user-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
+    UserRoutingModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
